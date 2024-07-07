0bba566892df997cf542288be8e32274deb5c5b4
FaridYoucef <songortekinn@gmail.com>
2024-07-07 11:28:46 +0100

    Setting Mushroom logic

--- src/utils/player.js
+++ src/utils/player.js
@@ -1,25 +1,24 @@
-
-export function addMario(currentLevel) {
+export function addMario() {
   let moveSpeed = 150;
   let jumpForce = 400;
   let currentJumpForce = jumpForce; // This ensures that Mario has a defined jump force
+  let enemySpeed = 80;
   let isJumping = true;
-  
-   
+
   const mario = add([
     sprite("mario"),
     pos(),
     area(),
-    body({stickToPltform: true}),
+    body({ stickToPltform: true }),
   ]);
-  
-  //Gravity
-  setGravity(1400) 
 
-  // Destroy evil mushroom
-mario.onCollide("dangrous", (d) => {
-    destroy(d)
-})
+  //Gravity
+  setGravity(1400);
+
+  // Destroy enemies
+  mario.onCollide("dangrous", (d) => {
+    destroy(d);
+  });
   // Move Mario
   onKeyDown("left", () => {
     mario.move(-moveSpeed, 0);
@@ -33,15 +32,40 @@ mario.onCollide("dangrous", (d) => {
     if (mario.isGrounded()) {
       mario.jump(currentJumpForce);
     }
-  }) 
-    
+  });
+
   // Camera following Mario
   mario.onUpdate(() => {
-    camPos(mario.pos)
-    if(mario.pos.y >= 400){
-      go('lose')
-     }
-  })
+    camPos(mario.pos);
+    if (mario.pos.y >= 400) {
+      go("lose");
+    }
+  });
+
+  //Mushroom logic
+  mario.onCollide("mushroom-surprise", (obj) => {
+    if (obj) {
+      const mushroomPos = obj.pos.sub(0, obj.height);
+      let mushroom = add([
+        sprite("mushroom"),
+        pos(mushroomPos),
+        area(),
+        body(),
+        "mushroom",
+      ]);
+      obj.use(sprite("unboxed"));
+
+      //Move Mushroom
+      mushroom.onUpdate(() => {
+        mushroom.move(70, 0);
+      });
+
+      //Destroy mushroom
+      mario.onCollide("mushroom", (m) => {
+        destroy(m);
+      });
+    }
+  });
+  
   return mario;
 }
-

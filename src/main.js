8e2f040b9826e3b2ed4f747e18deeaa952c6054e
FaridYoucef <songortekinn@gmail.com>
2024-07-07 07:20:32 +0100

    initialising the score and adding the lose scene

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import kaplay from "kaplay";
+import kaboom from "kaplay";
 import { load } from "./loader";
 import {  Level} from "./utils/level";
 import {addMario} from './utils/player'
@@ -6,7 +6,7 @@ import {addMario} from './utils/player'
 
 
 
-kaplay({
+kaboom({
    scale:1.3,
    background: [ 0, 0, 255, ],
 });
@@ -19,23 +19,31 @@ load.assets();
 
 scene("game", () => {
 
-// Add level
-const map = levelManager.getMap(1); // Change to 1 for the second map
-const levelCfg = levelManager.getLevelConfig();
+    // Add level
+    const map = levelManager.getMap(); // Change to 1 for the second map
+    const levelCfg = levelManager.getLevelConfig();
 
-const gameLevel = addLevel(map, {
-  width: levelCfg.tileWidth,
-  height: levelCfg.tileHeight,
-  ...levelCfg,
+    const gameLevel = addLevel(map, {
+      width: levelCfg.tileWidth,
+      height: levelCfg.tileHeight,
+      ...levelCfg,
+    });
+
+    // Add the player
+    addMario()
+
+    // Render the score lable
+    let score = 0;
+    
+    const scoreLable = add([text(`Score:${score}`), pos(24, 24) ]);
+
+  
+ 
+   // Lose scene
+   scene('lose', () => { 
+    add([text('Game Over'), pos(width() / 2, height()/ 2 - 80), scale(2), anchor('center')])
+})
+ 
 });
 
-// Add the player
-addMario()
- // Render the score lable
- add([text("Score: 0"), pos(24, 24), { value: 0 }]);
- 
- 
- 
-});
-
-go("game");
+go("game", 0);
